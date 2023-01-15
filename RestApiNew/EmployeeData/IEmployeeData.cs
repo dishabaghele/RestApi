4240dd32cb200786798b26f1556a97613cbb3419
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2023-01-15 15:11:15 +0530

    Add files via upload

---
+++ RestApiNew/EmployeeData/IEmployeeData.cs
@@ -0,0 +1,19 @@
+﻿using RestApiNew.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RestApiNew.EmployeeData
+{
+     public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid id);
+
+        Employee AddEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
+    }
+}
---
+++ RestApiNew/EmployeeData/MockEmployeeData.cs
@@ -0,0 +1,54 @@
+﻿using RestApiNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RestApiNew.EmployeeData
+{
+    public class MockEmployeeData : IEmployeeData
+    {
+        private List<Employee> employees = new List<Employee>()
+        {
+            new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Employee One"
+            },
+            new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Employee Two"
+            }
+        };
+
+        public Employee AddEmployee(Employee employee)
+        {
+            employee.Id = Guid.NewGuid();
+            employees.Add(employee);
+            return employee;
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            employees.Remove(employee);
+        }
+
+        public Employee EditEmployee(Employee employee)
+        {
+            var ExistingEmployee = GetEmployee(employee.Id);
+            ExistingEmployee.Name = employee.Name;
+            return ExistingEmployee;
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return employees.SingleOrDefault(x => x.Id == id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+    }
+}
